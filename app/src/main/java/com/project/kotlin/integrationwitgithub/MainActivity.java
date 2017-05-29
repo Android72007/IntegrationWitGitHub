f36c7142deacee6271854ca9c6eb2f41bd70b3c8
Ravi Tej Kallepalli <k.ravitej4u@gmail.com>
2017-05-29 15:58:06 -0400

    Initial commit

---
+++ app/src/main/java/com/project/kotlin/integrationwitgithub/MainActivity.java
@@ -0,0 +1,18 @@
+package com.project.kotlin.integrationwitgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void increment(){
+        int x = 0;
+        x=x+1;
+    }
+}
