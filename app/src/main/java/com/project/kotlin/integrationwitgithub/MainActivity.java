a42b543eea6827d7202ea0bd36691912ce8bb0ba
ravi tej <k.ravitej4u@gmail.com>
2017-06-03 18:40:14 -0400

    added methods back ravi tej

--- app/src/main/java/com/project/kotlin/integrationwitgithub/MainActivity.java
+++ app/src/main/java/com/project/kotlin/integrationwitgithub/MainActivity.java
@@ -17,4 +17,26 @@ public class MainActivity extends AppCompatActivity {
         x=x+4;
     }
 
+    public void decrement(){
+        int x = 0;
+        x=x+1;
+        x=x+4;
+    }
+
+    public void incdec(){
+        int x = 0;
+        x=x+1;
+        x=x+4;
+    }
+
+    public void inc(){
+        int x = 0;
+        x=x+1;
+        x=x+4;
+    }
+    public void dec(){
+        int x = 0;
+        x=x+1;
+        x=x+43;
+    }
 }
